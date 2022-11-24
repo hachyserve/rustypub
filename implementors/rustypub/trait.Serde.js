c4557fabc0fd28ebc1015ea8ed60bd50f7437bf4
dmah42 <510002+dmah42@users.noreply.github.com>
2022-11-24 14:41:12 +0000

    Deploying to gh-pages from @ hachyserve/rustypub@ed74a6346772073e508f8c64fc776768fe332597 🚀

--- implementors/rustypub/trait.Serde.js
+++ implementors/rustypub/trait.Serde.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["rustypub"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"rustypub":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
